c3ed1d280afbb7013f879f7b3a98fdabbd2f53a2
Sheng Luo <luosheng1986@gmail.com>
2012-11-22 11:52:58 +0800

    Set LSViewcontroller's backing view to LSEmojiView.

---
+++ CoreTextEmoji/LSEmojiView.h
@@ -0,0 +1,13 @@
+//
+//  LSEmojiView.h
+//  CoreTextEmoji
+//
+//  Created by Luo Sheng on 12-11-22.
+//  Copyright (c) 2012年 CodeLeaks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LSEmojiView : UIView
+
+@end
---
+++ CoreTextEmoji/LSEmojiView.m
@@ -0,0 +1,31 @@
+//
+//  LSEmojiView.m
+//  CoreTextEmoji
+//
+//  Created by Luo Sheng on 12-11-22.
+//  Copyright (c) 2012年 CodeLeaks. All rights reserved.
+//
+
+#import "LSEmojiView.h"
+
+@implementation LSEmojiView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
