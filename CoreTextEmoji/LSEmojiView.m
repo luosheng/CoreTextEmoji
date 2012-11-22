557d9b250ee7590161b00a099c15f6293bea22f3
Sheng Luo <luosheng1986@gmail.com>
2012-11-22 15:46:48 +0800

    Show how to do user-defined font cascading.

--- CoreTextEmoji/LSEmojiView.m
+++ CoreTextEmoji/LSEmojiView.m
@@ -36,18 +36,38 @@
 	CGContextSetTextMatrix(context, CGAffineTransformMakeScale(1.0, -1.0));
 	CGContextSetTextPosition(context, 10, 50);
 	CTLineDraw(line, context);
+	CFRelease(line);
 	
 	NSString *string2 = [string stringByAddingZeroWidthSpacesAfterEmojiCharacters];
 	NSAttributedString *attributedString2 = [[NSAttributedString alloc] initWithString:string2 attributes:attributes];
 	CTLineRef line2 = CTLineCreateWithAttributedString((__bridge CFAttributedStringRef)(attributedString2));
 	CGContextSetTextPosition(context, 10, 100);
 	CTLineDraw(line2, context);
+	CFRelease(line2);
 	
 	NSDictionary *attributes3 = @{(id)kCTFontAttributeName : [UIFont fontWithName:@"AppleColorEmoji" size:FONT_SIZE]};
 	NSAttributedString *attributedString3 = [[NSAttributedString alloc] initWithString:string attributes:attributes3];
 	CTLineRef line3 = CTLineCreateWithAttributedString((__bridge CFAttributedStringRef)(attributedString3));
 	CGContextSetTextPosition(context, 10, 150);
 	CTLineDraw(line3, context);
+	CFRelease(line3);
+	
+	NSDictionary *fontAttributes = @{
+		(id)kCTFontFamilyNameAttribute : @"Georgia",
+		(id)kCTFontCascadeListAttribute : @[
+			(__bridge id)CTFontDescriptorCreateWithNameAndSize(CFSTR("AppleColorEmoji"), 0),
+			(__bridge id)CTFontDescriptorCreateWithNameAndSize(CFSTR("STHeitiSC-Medium"), 0)
+		]
+	};
+	CTFontDescriptorRef descriptor = CTFontDescriptorCreateWithAttributes((__bridge CFDictionaryRef)(fontAttributes));
+	CTFontRef font = CTFontCreateWithFontDescriptor(descriptor, FONT_SIZE, 0);
+	NSDictionary *attributes4 = @{(id)kCTFontAttributeName : (__bridge id)font};
+	CFRelease(font);
+	NSAttributedString *attributedString4 = [[NSAttributedString alloc] initWithString:string attributes:attributes4];
+	CTLineRef line4 = CTLineCreateWithAttributedString((__bridge CFAttributedStringRef)(attributedString4));
+	CGContextSetTextPosition(context, 10, 200);
+	CTLineDraw(line4, context);
+	CFRelease(line4);
 }
 
 @end
